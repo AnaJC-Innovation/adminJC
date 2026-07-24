19fdf59a930d7b300f378ab6b0fe430018aa9e89
anacruz <acruz@jc-innovation.com>
2026-07-24 13:47:45 -0600

    perfils

--- resources/views/profile/partials/main.blade.php
+++ resources/views/profile/partials/main.blade.php
@@ -4,71 +4,83 @@
             <div class="nav-align-top">
                 <ul class="nav nav-pills flex-column flex-md-row mb-6 gap-md-0 gap-2">
                     <li class="nav-item">
-                        <a class="nav-link active" href="javascript:void(0);"><i class="icon-base ti tabler-users icon-sm me-1_5"></i> Account</a>
+                        <a class="nav-link active" href="javascript:void(0);"><i class="icon-base ti tabler-users icon-sm me-1_5"></i> Perfil</a>
                     </li>
                 </ul>
             </div>
             <div class="card mb-6">
                 <!-- Account -->
                 <div class="card-body">
-                    <div class="d-flex align-items-start align-items-sm-center gap-6">
-                        <img
-                            src="../../assets/img/avatars/1.png"
-                            alt="user-avatar"
-                            class="d-block w-px-100 h-px-100 rounded"
-                            id="uploadedAvatar" />
-                        <div class="button-wrapper">
-                            <label for="upload" class="btn btn-primary me-3 mb-4" tabindex="0">
-                                <span class="d-none d-sm-block">Upload new photo</span>
-                                <i class="icon-base ti tabler-upload d-block d-sm-none"></i>
-                                <input
-                                    type="file"
-                                    id="upload"
-                                    class="account-file-input"
-                                    hidden
-                                    accept="image/png, image/jpeg" />
-                            </label>
-                            <button type="button" class="btn btn-label-secondary account-image-reset mb-4">
-                                <i class="icon-base ti tabler-reset d-block d-sm-none"></i>
-                                <span class="d-none d-sm-block">Reset</span>
-                            </button>
+                    <form id="formAccountSettings" method="POST" action="{{ route('profile.updatesave') }}">
+                        @csrf
+                        <?php
+                        $url = '1.png';
+                        if (auth()->user()->photo != '') {
+                            $url = auth()->user()->photo;
+                        }
+                        ?>
 
-                            <div>Allowed JPG, GIF or PNG. Max size of 800K</div>
+                        <div class="d-flex align-items-start align-items-sm-center gap-6">
+                            <img
+                                src="{{ asset('photos/' . $url) }}"
+                                alt="user-avatar"
+                                class="d-block w-px-100 h-px-100 rounded"
+                                id="uploadedAvatar" />
+                            <div class="button-wrapper">
+                                <label for="upload" class="btn btn-primary me-3 mb-4" tabindex="0">
+                                    <span class="d-none d-sm-block">Subir foto</span>
+                                    <i class="icon-base ti tabler-upload d-block d-sm-none"></i>
+                                    <input
+                                        type="file"
+                                        id="upload"
+                                        class="account-file-input"
+                                        hidden
+                                        accept="image/png, image/jpeg" />
+                                </label>
+                                <button type="button" class="btn btn-label-secondary account-image-reset mb-4">
+                                    <i class="icon-base ti tabler-reset d-block d-sm-none"></i>
+                                    <span class="d-none d-sm-block">Reiniciar</span>
+                                </button>
+                                <div>Permitido JPG, GIF o PNG. Tamaño máximo de 800K</div>
+                            </div>
                         </div>
-                    </div>
                 </div>
                 <div class="card-body pt-4">
-                    <form id="formAccountSettings" method="GET" onsubmit="return false">
-                        <div class="row gy-4 gx-6 mb-6">
-                            <div class="col-md-6 form-control-validation">
-                                <label for="firstName" class="form-label">First Name</label>
-                                <input
-                                    class="form-control"
-                                    type="text"
-                                    id="firstName"
-                                    name="firstName"
-                                    value="John"
-                                    autofocus />
-                            </div>
-                            <div class="col-md-6 form-control-validation">
-                                <label for="lastName" class="form-label">Last Name</label>
-                                <input class="form-control" type="text" name="lastName" id="lastName" value="Doe" />
-                            </div>
-                            <div class="col-md-6">
-                                <label for="email" class="form-label">E-mail</label>
-                                <input
-                                    class="form-control"
-                                    type="text"
-                                    id="email"
-                                    name="email"
-                                    value="john.doe@example.com"
-                                    placeholder="john.doe@example.com" />
-                            </div>
+                    <div class="row gy-4 gx-6 mb-6">
+                        <div class="col-md-6 form-control-validation">
+                            <label for="firstName" class="form-label">Nombre</label>
+                            <input
+                                class="form-control"
+                                type="text"
+                                id="firstName"
+                                name="firstName"
+                                value="{{auth()->user()->name}}"
+                                autofocus />
                         </div>
-                        <div class="mt-2">
-                            <button type="submit" class="btn btn-primary me-3">Save changes</button>
-                            <button type="reset" class="btn btn-label-secondary">Cancel</button>
+                        <div class="col-md-6">
+                            <label for="email" class="form-label">E-mail</label>
+                            <input
+                                class="form-control"
+                                type="text"
+                                id="email"
+                                name="email"
+                                value="{{auth()->user()->email}}"
+                                placeholder="{{auth()->user()->email}}" />
                         </div>
+                        <div class="col-md-6">
+                            <label for="password" class="form-label">Contraseña</label>
+                            <input
+                                class="form-control"
+                                type="password"
+                                id="password"
+                                name="password"
+                                placeholder="Contraseña" />
+                        </div>
+                    </div>
+                    <div class="mt-2">
+                        <button type="submit" class="btn btn-primary me-3">Guardar</button>
+                        <button type="reset" class="btn btn-label-secondary">Cancelar</button>
+                    </div>
                     </form>
                 </div>
                 <!-- /Account -->
--- storage/framework/views/ff80b149d4e08d2849ba0e4c840966e6.php
+++ storage/framework/views/ff80b149d4e08d2849ba0e4c840966e6.php
@@ -4,76 +4,88 @@
             <div class="nav-align-top">
                 <ul class="nav nav-pills flex-column flex-md-row mb-6 gap-md-0 gap-2">
                     <li class="nav-item">
-                        <a class="nav-link active" href="javascript:void(0);"><i class="icon-base ti tabler-users icon-sm me-1_5"></i> Account</a>
+                        <a class="nav-link active" href="javascript:void(0);"><i class="icon-base ti tabler-users icon-sm me-1_5"></i> Perfil</a>
                     </li>
                 </ul>
             </div>
             <div class="card mb-6">
                 <!-- Account -->
                 <div class="card-body">
-                    <div class="d-flex align-items-start align-items-sm-center gap-6">
-                        <img
-                            src="../../assets/img/avatars/1.png"
-                            alt="user-avatar"
-                            class="d-block w-px-100 h-px-100 rounded"
-                            id="uploadedAvatar" />
-                        <div class="button-wrapper">
-                            <label for="upload" class="btn btn-primary me-3 mb-4" tabindex="0">
-                                <span class="d-none d-sm-block">Upload new photo</span>
-                                <i class="icon-base ti tabler-upload d-block d-sm-none"></i>
-                                <input
-                                    type="file"
-                                    id="upload"
-                                    class="account-file-input"
-                                    hidden
-                                    accept="image/png, image/jpeg" />
-                            </label>
-                            <button type="button" class="btn btn-label-secondary account-image-reset mb-4">
-                                <i class="icon-base ti tabler-reset d-block d-sm-none"></i>
-                                <span class="d-none d-sm-block">Reset</span>
-                            </button>
+                    <form id="formAccountSettings" method="POST" action="<?php echo e(route('profile.updatesave')); ?>">
+                        <?php echo csrf_field(); ?>
+                        <?php
+                        $url = '1.png';
+                        if (auth()->user()->photo != '') {
+                            $url = auth()->user()->photo;
+                        }
+                        ?>
 
-                            <div>Allowed JPG, GIF or PNG. Max size of 800K</div>
+                        <div class="d-flex align-items-start align-items-sm-center gap-6">
+                            <img
+                                src="<?php echo e(asset('photos/' . $url)); ?>"
+                                alt="user-avatar"
+                                class="d-block w-px-100 h-px-100 rounded"
+                                id="uploadedAvatar" />
+                            <div class="button-wrapper">
+                                <label for="upload" class="btn btn-primary me-3 mb-4" tabindex="0">
+                                    <span class="d-none d-sm-block">Subir foto</span>
+                                    <i class="icon-base ti tabler-upload d-block d-sm-none"></i>
+                                    <input
+                                        type="file"
+                                        id="upload"
+                                        class="account-file-input"
+                                        hidden
+                                        accept="image/png, image/jpeg" />
+                                </label>
+                                <button type="button" class="btn btn-label-secondary account-image-reset mb-4">
+                                    <i class="icon-base ti tabler-reset d-block d-sm-none"></i>
+                                    <span class="d-none d-sm-block">Reiniciar</span>
+                                </button>
+                                <div>Permitido JPG, GIF o PNG. Tamaño máximo de 800K</div>
+                            </div>
                         </div>
-                    </div>
                 </div>
                 <div class="card-body pt-4">
-                    <form id="formAccountSettings" method="GET" onsubmit="return false">
-                        <div class="row gy-4 gx-6 mb-6">
-                            <div class="col-md-6 form-control-validation">
-                                <label for="firstName" class="form-label">First Name</label>
-                                <input
-                                    class="form-control"
-                                    type="text"
-                                    id="firstName"
-                                    name="firstName"
-                                    value="John"
-                                    autofocus />
-                            </div>
-                            <div class="col-md-6 form-control-validation">
-                                <label for="lastName" class="form-label">Last Name</label>
-                                <input class="form-control" type="text" name="lastName" id="lastName" value="Doe" />
-                            </div>
-                            <div class="col-md-6">
-                                <label for="email" class="form-label">E-mail</label>
-                                <input
-                                    class="form-control"
-                                    type="text"
-                                    id="email"
-                                    name="email"
-                                    value="john.doe@example.com"
-                                    placeholder="john.doe@example.com" />
-                            </div>
+                    <div class="row gy-4 gx-6 mb-6">
+                        <div class="col-md-6 form-control-validation">
+                            <label for="firstName" class="form-label">Nombre</label>
+                            <input
+                                class="form-control"
+                                type="text"
+                                id="firstName"
+                                name="firstName"
+                                value="<?php echo e(auth()->user()->name); ?>"
+                                autofocus />
                         </div>
-                        <div class="mt-2">
-                            <button type="submit" class="btn btn-primary me-3">Save changes</button>
-                            <button type="reset" class="btn btn-label-secondary">Cancel</button>
+                        <div class="col-md-6">
+                            <label for="email" class="form-label">E-mail</label>
+                            <input
+                                class="form-control"
+                                type="text"
+                                id="email"
+                                name="email"
+                                value="<?php echo e(auth()->user()->email); ?>"
+                                placeholder="<?php echo e(auth()->user()->email); ?>" />
                         </div>
+                        <div class="col-md-6">
+                            <label for="password" class="form-label">Contraseña</label>
+                            <input
+                                class="form-control"
+                                type="password"
+                                id="password"
+                                name="password"
+                                placeholder="Contraseña" />
+                        </div>
+                    </div>
+                    <div class="mt-2">
+                        <button type="submit" class="btn btn-primary me-3">Guardar</button>
+                        <button type="reset" class="btn btn-label-secondary">Cancelar</button>
+                    </div>
                     </form>
                 </div>
                 <!-- /Account -->
             </div>
-            <div class="card">
+            <!-- <div class="card">
                 <h5 class="card-header">Delete Account</h5>
                 <div class="card-body">
                     <div class="mb-6 col-12 mb-0">
@@ -96,7 +108,7 @@
                         </button>
                     </form>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </div><?php /**PATH C:\Users\JC-INNOVATION\adminJC\adminJC\resources\views/profile/partials/main.blade.php ENDPATH**/ ?>
